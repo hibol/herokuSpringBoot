c8ee30b5f9f69e41b14b24bdb143a508c00e0ee1
Ludivine <l.ammon@avatarion.ch>
2022-07-25 14:49:37 +0200

    init

---
+++ src/main/java/com/hibol/herokkuapp/HerokuappApplication.java
@@ -0,0 +1,13 @@
+package com.hibol.herokkuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuappApplication.class, args);
+	}
+
+}
